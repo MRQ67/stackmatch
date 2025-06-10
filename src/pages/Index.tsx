d227d832541725d26ac162b2603af8954f7c327b
MRQ67 <kasaboody@gmail.com>
2025-06-10 18:43:06 +0300

    Adjusted Proper Logo

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -98,7 +98,7 @@ const Index = () => {
               <img 
                 src="/lovable-uploads/stackmatch_logo.png" 
                 alt="Stackmatch Logo" 
-                className="h-17 w-25"
+                className="h-12 w-auto"
               />
             </div>
             
